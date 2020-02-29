239b39050ae6c00119bd93486a20868953222619
Daniel Huisman <daniel@huisman.me>
2020-02-29 20:14:56 +0100

    Add accepted mandate counter to members page

--- src/pages/members/Members.tsx
+++ src/pages/members/Members.tsx
@@ -7,7 +7,8 @@ import {Container, Header, Loader, Table} from 'semantic-ui-react';
 import TableSelectableRow from '../../components/table/TableSelectableRow';
 import YesNo from '../../components/util/YesNo';
 import GetMembers from '../../queries/GetMembers.graphql';
-import {GetMembers as GetMembersType, MandateStatus} from '../../types/generatedTypes';
+import {GetMembers as GetMembersType} from '../../types/generatedTypes';
+import {hasAcceptedMandates} from '../../util';
 
 const Members = () => {
     const {t} = useTranslation();
@@ -25,32 +26,39 @@ const Members = () => {
             {loading && <Loader active />}
 
             {data && (
-                <Table selectable stackable>
-                    <Table.Header>
-                        <Table.Row>
-                            <Table.HeaderCell>{t('members:member.firstName', 'First name')}</Table.HeaderCell>
-                            <Table.HeaderCell>{t('members:member.initials', 'Initials')}</Table.HeaderCell>
-                            <Table.HeaderCell>{t('members:member.lastName', 'Last name')}</Table.HeaderCell>
-                            <Table.HeaderCell>{t('members:member.email', 'Email address')}</Table.HeaderCell>
-                            <Table.HeaderCell>{t('members:member.language', 'Language')}</Table.HeaderCell>
-                            <Table.HeaderCell>{t('members:member.hasMandate', 'Has mandate')}</Table.HeaderCell>
-                        </Table.Row>
-                    </Table.Header>
-                    <Table.Body>
-                        {data.members.map((member) => (
-                            <TableSelectableRow key={member.id} to={`/members/${member.id}`}>
-                                <Table.Cell>{member.firstName}</Table.Cell>
-                                <Table.Cell>{member.initials}</Table.Cell>
-                                <Table.Cell>{member.lastName}</Table.Cell>
-                                <Table.Cell>{member.email}</Table.Cell>
-                                <Table.Cell>{t(`members:member.languages.${member.language}`)}</Table.Cell>
-                                <Table.Cell>
-                                    <YesNo value={member.mandates.filter((mandate) => mandate.status === MandateStatus.ACCEPTED).length > 0} />
-                                </Table.Cell>
-                            </TableSelectableRow>
-                        ))}
-                    </Table.Body>
-                </Table>
+                <>
+                    <p>{
+                        t('members:members.withMandate', 'Members with mandate:')}
+                        {' '}
+                        {data.members.filter(hasAcceptedMandates).length} / {data.members.length}
+                    </p>
+                    <Table selectable stackable>
+                        <Table.Header>
+                            <Table.Row>
+                                <Table.HeaderCell>{t('members:member.firstName', 'First name')}</Table.HeaderCell>
+                                <Table.HeaderCell>{t('members:member.initials', 'Initials')}</Table.HeaderCell>
+                                <Table.HeaderCell>{t('members:member.lastName', 'Last name')}</Table.HeaderCell>
+                                <Table.HeaderCell>{t('members:member.email', 'Email address')}</Table.HeaderCell>
+                                <Table.HeaderCell>{t('members:member.language', 'Language')}</Table.HeaderCell>
+                                <Table.HeaderCell>{t('members:member.hasMandate', 'Has mandate')}</Table.HeaderCell>
+                            </Table.Row>
+                        </Table.Header>
+                        <Table.Body>
+                            {data.members.map((member) => (
+                                <TableSelectableRow key={member.id} to={`/members/${member.id}`}>
+                                    <Table.Cell>{member.firstName}</Table.Cell>
+                                    <Table.Cell>{member.initials}</Table.Cell>
+                                    <Table.Cell>{member.lastName}</Table.Cell>
+                                    <Table.Cell>{member.email}</Table.Cell>
+                                    <Table.Cell>{t(`members:member.languages.${member.language}`)}</Table.Cell>
+                                    <Table.Cell>
+                                        <YesNo value={hasAcceptedMandates(member)} />
+                                    </Table.Cell>
+                                </TableSelectableRow>
+                            ))}
+                        </Table.Body>
+                    </Table>
+                </>
             )}
         </Container>
     );
